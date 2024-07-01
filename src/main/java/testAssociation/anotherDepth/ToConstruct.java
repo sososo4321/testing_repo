05600238725a5546e4a9286e8d1658f70e152681
sophie <sophie.vanderdong@gmail.com>
2024-07-01 21:10:54 +0200

    add class

---
+++ src/main/java/testAssociation/anotherDepth/ToConstruct.java
@@ -0,0 +1,11 @@
+package testAssociation.anotherDepth;
+
+import testAssociation.Food;
+
+public class ToConstruct {
+    Food feed;
+    String cons = "constrict";
+    public ToConstruct(Food feed) {
+        this.feed = feed;
+    }
+}
