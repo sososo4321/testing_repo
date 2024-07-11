99dfb579f742daf001bdb86aa0ca1509414d77ce
sophie <sophie.vanderdong@gmail.com>
2024-07-11 18:23:48 +0200

    to check the exclusion of certain types and external class creation.

--- src/main/java/tempTests/Wheel.java
+++ src/main/java/tempTests/Wheel.java
@@ -1,5 +1,13 @@
 package tempTests;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import testClasses.testPaths.*;
+
 public class Wheel {
 Car car = new Car();
+ArrayList<String> hello = new ArrayList<>();
+HashMap<Integer,String> helloMap = new HashMap();
+HashMap<String,ArrayList> helloMany;
+Baby helloBaby;
 }
